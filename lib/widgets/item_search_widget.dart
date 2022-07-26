2f91bcc9acdb6a5b03a2a2d8ea936b9574033914
reynaldo57 <reynaldo57@hotmail.com>
2022-07-25 23:23:09 -0500

    CategoryPage2

---
+++ lib/widgets/item_search_widget.dart
@@ -0,0 +1,103 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class ItemSearchWidget extends StatelessWidget {
+  const ItemSearchWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+      child: Row(
+        children: [
+          Container(
+            height: 100.0,
+            width: 100.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14.0),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://comidasperuanas.net/wp-content/uploads/2020/12/Torta-Dona-Pepa.jpg"))),
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Torta de Chocolate",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0),
+                  ),
+                  SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(
+                    "Torta de Chocolate y nueces con manjar acompañado de frutas de estacion",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14.0),
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Color(0xffED9B00),
+                        size: 16.0,
+                      ),
+                      Text(
+                        "4.5",
+                        style: TextStyle(
+                            color: Colors.white60,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Icon(
+                        Icons.timer,
+                        color: Colors.white60,
+                        size: 16.0,
+                      ),
+                      SizedBox(
+                        width: 2.0,
+                      ),
+                      Text(
+                        "2min",
+                        style: TextStyle(
+                            color: Colors.white60,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14.0),
+                      ),
+                    ],
+                  )
+                ],
+              )),
+          Text(
+            "S/ 10.00",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
