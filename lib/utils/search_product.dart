432336db18c0d435ef6740bfad17d4d2e0950b2f
reynaldo57 <reynaldo57@hotmail.com>
2022-08-06 19:05:31 -0500

    searchProduct22

--- lib/utils/search_product.dart
+++ lib/utils/search_product.dart
@@ -1,39 +1,27 @@
-
-
-
 import 'package:flutter/material.dart';
 
-class SearchProduct extends SearchDelegate{
-
+class SearchProduct extends SearchDelegate {
   String result = "";
 
-
-  List<String> names = [
-    "Camila chapman", "Belinda Cameron"
-
-  ];
-
-
-
+  List<String> names = ["Camila chapman", "Belinda Cameron"];
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return[
+    return [
       IconButton(
         icon: Icon(Icons.close),
-        onPressed: (){
+        onPressed: () {
           query = "";
         },
       )
     ];
-
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
       icon: Icon(Icons.arrow_back),
-      onPressed: (){
+      onPressed: () {
         close(context, result);
       },
     );
@@ -41,34 +29,37 @@ class SearchProduct extends SearchDelegate{
 
   @override
   Widget buildResults(BuildContext context) {
-    return ListView.builder(
-      itemCount: 2,
-      itemBuilder: (BuildContext, int index){
-        return ListTile(
-          title: Text("Hola"),
-        );
-      },
-    );
-}
-
-@override
-Widget buildSuggestions(BuildContext context) {
-    
-    List<String> suggestions = names.where((element){
+    List<String> suggestions = names.where((element) {
       return element.toLowerCase().contains(query.toLowerCase());
     }).toList();
 
     print(suggestions);
-    
-    
-  return ListView.builder(
-    itemCount: 10,
-    itemBuilder: (BuildContext, int index){
-      return ListTile(
-        title: Text("Hola"),
-      );
-    },
-  );
-}
 
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (BuildContext, int index) {
+        return ListTile(
+          title: Text(suggestions[index]),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = names.where((element) {
+      return element.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
+    print(suggestions);
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (BuildContext, int index) {
+        return ListTile(
+          title: Text(suggestions[index]),
+        );
+      },
+    );
+  }
 }
