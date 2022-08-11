b2397b9844d8feed705e07dca47edf56f079039e
reynaldo57 <reynaldo57@hotmail.com>
2022-08-10 21:59:30 -0500

    ProductPage2

--- lib/pages/admin/product_admin_page.dart
+++ lib/pages/admin/product_admin_page.dart
@@ -1,11 +1,15 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:restaurant/services/firestore_service.dart';
 
 class ProductAdminPage extends StatelessWidget {
 
+  FirestoreService _firestoreService = new FirestoreService(collection: ("products"));
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Productos"),
@@ -16,7 +20,39 @@ class ProductAdminPage extends StatelessWidget {
         },
         child: Icon(Icons.add),
       ),
-      body: Center(),
+      body: FutureBuilder(
+        future: _firestoreService.getAllProducts(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if (snap.hasData){
+            List<Map<String, dynamic>> products = snap.data;
+            return ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (BuildContext context, int index){
+                return Card(
+                  child: ListTile(
+                    title: Text(products[index]["name"]),
+                    subtitle: Text(
+                      products[index]["description"],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
