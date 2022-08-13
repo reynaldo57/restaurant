23bb91959fee9ab8325cc92220433e2a149f6890
reynaldo57 <reynaldo57@hotmail.com>
2022-08-12 21:21:50 -0500

    ProductPage5

--- lib/pages/admin/product_add_edit_admin_page.dart
+++ lib/pages/admin/product_add_edit_admin_page.dart
@@ -6,6 +6,7 @@ class ProductAddEditAdminPage extends StatelessWidget {
 
   final _formKey = GlobalKey<FormState>();
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +39,96 @@ class ProductAddEditAdminPage extends StatelessWidget {
                     }
                     return null;
                   },
-                )
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Descripcion",
+                      hintText: "Descripcion del Producto"
+                  ),
+                  validator: (String? value){
+                    if(value!.isEmpty || value == null){
+                      return "Completar el campo";
+                    }
+                    return null;
+                  },
+                ),
+                DropdownButtonFormField(
+                  decoration: InputDecoration(
+                      labelText: "Categoria",
+                      hintText: "--Selecciona una Categoria"
+                  ),
+                  value: "Hola",
+                  onChanged: (String? value){
+                    print(value);
+                  },
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("Hola"),
+                      value: "Hola",
+                      onTap: (){
+
+                      },
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Hola 2"),
+                      value: "Hola 2",
+                      onTap: (){
+
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Precio",
+                      hintText: "Precio del Producto"
+                  ),
+                  validator: (String? value){
+                    if(value!.isEmpty || value == null){
+                      return "Completar el campo";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Tiempo",
+                      hintText: "Tiempo del Producto"
+                  ),
+                  validator: (String? value){
+                    if(value!.isEmpty || value == null){
+                      return "Completar el campo";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Puntaje",
+                      hintText: "Puntaje del Producto"
+                  ),
+                  validator: (String? value){
+                    if(value!.isEmpty || value == null){
+                      return "Completar el campo";
+                    }
+                    return null;
+                  },
+                ),
               ],
             ),
           ),
