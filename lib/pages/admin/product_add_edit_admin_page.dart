0eb798eb63617dab106c4102cbee5ed963986f43
reynaldo57 <reynaldo57@hotmail.com>
2022-08-12 20:36:19 -0500

    ProductPage4

--- lib/pages/admin/product_add_edit_admin_page.dart
+++ lib/pages/admin/product_add_edit_admin_page.dart
@@ -4,13 +4,46 @@ import 'package:flutter/material.dart';
 
 class ProductAddEditAdminPage extends StatelessWidget {
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Gestionar Productos"),
+        title: Text("Agregar Productos"),
       ),
-      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          if(_formKey.currentState!.validate()){
+
+          }
+        },
+        child: Icon(Icons.save),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Producto",
+                    hintText: "Nombre del Producto"
+                  ),
+                  validator: (String? value){
+                    if(value!.isEmpty || value == null){
+                      return "Completar el campo";
+                    }
+                    return null;
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
